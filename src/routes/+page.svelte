f09e6d162bf61b8e6c2fe6cde81f5ac4f42efb7c
Sean Parkin <sdparkin@protonmail.com>
2025-06-01 21:50:46 -0500

    Changed the text from the default

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to This site!!!</h1>
+<p>Some really interesting text will go here later!!</p>
